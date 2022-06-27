f6c43fcf758d6da2c68b64a3f5efdc229c8aaf46
IPLOID <40538508+IPLOID@users.noreply.github.com>
2022-06-28 00:03:17 +0300

    Add files via upload

---
+++ Work 1.1 updated.py
@@ -0,0 +1,15 @@
+def domain_name(url):
+    length = len(url)
+    s = url
+    su = url
+    for i in range(length) :
+        if url[i] == ":" :
+            s = url[i+3:length:1]
+        if url[i:i+4:1] == "www.":
+            s = url[i+4:length:1]        
+    length = len(s)
+    for i in range(length-1) :
+        if s[i] == "." :
+            su = s[0:i:1]
+            break
+    return su
